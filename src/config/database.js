b05a5b039a2e2a8fd1c5e740963d0fe1d367da16
“Renato <witcarerenato@gmail.com>
2020-06-26 12:42:03 -0300

    Criado pastas

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    dialect: 'mysql',
+    host: 'localhost',
+    port: '8889',
+    username: 'root',
+    password: 'root',
+    database: 'witcare',
+    define:{
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+
+};
